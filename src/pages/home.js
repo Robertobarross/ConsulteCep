7b5a9a78aa2d6b505346be034121d4ebc700d029
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-08-01 00:01:28 -0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,75 +1,26 @@
-import React, { useState } from 'react';
-import { Text, View, SafeAreaView, TextInput, TouchableOpacity, Image } from 'react-native';
-import axios from 'axios'; 
-import { styles } from './styles';
-import localizacao from '../imgs/localizacao.png';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { styles2 } from './styles2';
 
-export default function Home() {
-    const [infoCep, setInfoCep] = useState({});
-    const [searchCep, setSearchCep] = useState('');
-
-    const getCep = async () => {
-        const {data} = await axios.get(`https://viacep.com.br/ws/${searchCep}/json/`);
-        setInfoCep(data);
-    };
-
+export default function Home({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-    <View>
-       <Image source={localizacao} style={{ width: 150, height: 150 }} /> 
-    </View>
-    
-    <View>
-      <TextInput
-      placeholder='Cep, Ex: 01001000'
-      value={searchCep}
-      onChangeText={text => setSearchCep(text)}
-      keyboard
-      style={styles.cep}
-      />
+    <View style={styles2.container}>
+      <Text style={styles2.title}>
+        Consulte Cep
+      </Text>
+      <Text style={styles2.text}>
+        Olá, digite o Cep no formato indicado. Ex: 19135000. 
+        Sem a barra que divide os três ultimos números.
+        Click no botão a baixo para proceguir.
+      </Text>
 
-      <TouchableOpacity onPress={getCep} style={styles.botao}>
-        <Text style={styles.botaoText}>BUSCAR</Text>
-      </TouchableOpacity>
+      <Text
+        style={styles2.botao}
+        onPress={() => navigation.navigate('Dashboard')}>
+        Click aqui
+      </Text> 
+      
+      <StatusBar style="auto" />
     </View>
-
-    <View>
-        <View>
-            <Text style={styles.campos}>
-                Rua: {infoCep.logradouro}
-            </Text>
-        </View>
-        <View>
-            <Text style={styles.campos}>
-                Complemento: {infoCep.complemento}
-            </Text>
-        </View>
-        <View>
-            <Text style={styles.campos}>
-                Bairro: {infoCep.bairro}
-            </Text>
-        </View>
-        <View>
-            <Text style={styles.campos}>
-                Cidade: {infoCep.localidade}
-            </Text>
-        </View>
-        <View>
-            <Text style={styles.campos}>
-                Estado: {infoCep.uf}
-            </Text>
-        </View>
-        <View>
-            <Text style={styles.campos}>
-                ibge: {infoCep.ibge}
-            </Text>
-        </View>
-        <View>
-            <Text style={styles.campos}>
-                DDD: {infoCep.ddd}
-            </Text>
-        </View>
-    </View>
-    </SafeAreaView>
   );
 }
